1e81bc007e6978be94ff852b6bcca5d9fdd3dcdc
YoussefHussien99 <51840033+youssefa111@users.noreply.github.com>
2020-05-25 21:35:34 +0200

    Add files via upload

---
+++ Categories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int Categories_ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "catrgory")]
+        public string Categories_Name { get; set; }
+
+        public int number_of_products { get; set; }
+
+        public virtual ICollection<Ads> ads { get; set; }
+    }
+}
