b651667c58f0fc45ab06f349874a8730dbf05a8b
mohamedtamer92 <32773509+mohamedtamer92@users.noreply.github.com>
2020-05-25 22:05:43 +0200

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int Admin_Id { get; set; }
+
+
+        [Display(Name="Name")]
+        [EmailAddress]
+        [Required(ErrorMessage = "this field is requried")]
+        public string Admin_UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "this field is requried")]
+        public string Admin_Pass { get; set; }
+
+        public int RoleModel { get; set; }
+
+        public Admin()
+        {
+            RoleModel = 1;
+        }
+    }
+}
