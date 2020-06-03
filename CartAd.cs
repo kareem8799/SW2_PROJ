f7fb3367a75b196a487622d2afb402bfe1bdd80a
AhmedHFCI <42712655+AhmedHFCI@users.noreply.github.com>
2020-06-03 19:01:14 +0200

    Add files via upload

---
+++ CartAd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication5.Models
+{
+    public class CartAd
+    {
+
+        [Key,Column(Order=0)]
+        [ForeignKey("cart")]
+        public int CartId { get; set; }
+        [Key, Column(Order = 1)]
+        [ForeignKey("ad")]
+        public int AdId { get; set; }
+
+        public virtual Cart cart { get; set; }
+        public virtual Ads ad { get; set; }
+    }
+}
