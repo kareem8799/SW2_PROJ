a45434e1722e8ab81e9a994431fb1bb69cf42e1f
kareem8799 <37982588+kareem8799@users.noreply.github.com>
2020-06-03 19:06:41 +0200

    Add files via upload

---
+++ Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication5.Models
+{
+    public class Cart
+    {
+        [Key]
+        [ForeignKey("user")]
+        public int idofcart { get; set; }
+        
+        public virtual User user { get; set; }
+        public virtual ICollection<CartAd> cartad { get; set; }
+    }
+}
