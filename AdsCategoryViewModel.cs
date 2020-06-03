054fc40b03573578268555ea936a5bd2ab8418ad
mohamedtamer92 <32773509+mohamedtamer92@users.noreply.github.com>
2020-06-03 19:10:08 +0200

    Add files via upload

---
+++ AdsCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication5.Models;
+
+namespace WebApplication5.ViewModel
+{
+    public class AdsCategoryViewModel
+    {
+        public IEnumerable<Categories> Category { get; set; }
+        public Ads Ad { get; set; }
+    }
+}
